efaa92be687dbaef566796dc91774748f373acd1
ARALJO <araljo_1230@hotmail.com>
2022-10-12 17:45:34 -0500

    Arevalo

--- src/com/deportivo/controller/Conexion.java
+++ src/com/deportivo/controller/Conexion.java
@@ -5,9 +5,9 @@ import java.sql.*;
 
 public class Conexion {
     
-    private final String URL = "jdbc:postgresql://localhost:5432/bd_deportivo";
+    private final String URL = "jdbc:postgresql://localhost:5400/bd_deportivo";
     private final String USER = "postgres";
-    private final String PASS = "admin";
+    private final String PASS = "AmySellene2020";
     
     public Connection conectar(){
         
